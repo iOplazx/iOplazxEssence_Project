5b41ab874f305fe4504ff49b31e35bc6e317cd32
iOplazx <ioplazx.dev@proton.me>
2026-04-11 17:19:20 -0500

    A 'print' was commented on

--- _static/GameSaveManager.gd
+++ _static/GameSaveManager.gd
@@ -8,7 +8,7 @@ const GAME_NAME_CLASS = "GameSaveManager-USER"
 func _ready():
 	super._ready()
 	# This is where the addon confirms it is ready to receive save data
-	print("GameSaveManager: Persistence system ready for the Demo.")
+	#print("GameSaveManager: Persistence system ready for the Demo.")
 	var log_msg = "[%s/_ready] GameSaveManager initialized, wrapping EssenceSaveManager." % GAME_NAME_CLASS
 	EssenceLogger.system_info(log_msg)
 
