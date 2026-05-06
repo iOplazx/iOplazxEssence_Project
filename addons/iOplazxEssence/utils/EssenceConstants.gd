e661f04c1f26b0967925f604bff1bb47c5c83f9c
iOplazx <ioplazx.dev@proton.me>
2026-05-06 12:30:24 -0500

    v0.1.2a

--- addons/iOplazxEssence/utils/EssenceConstants.gd
+++ addons/iOplazxEssence/utils/EssenceConstants.gd
@@ -1,5 +1,5 @@
 class_name EssenceConstants
 
 const ENGINE_NAME = "iOplazxEssence Engine"
-const ENGINE_VERSION = "0.1.2"
-const UPDATE_DATE = "April 2026"
+const ENGINE_VERSION = "0.1.2a"
+const UPDATE_DATE = "May 2026"
